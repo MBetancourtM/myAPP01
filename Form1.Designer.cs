419c13718aab0a7ae19f9e15ecd2eef02a1a40a0
Paco9CG <alu.25130444@correo.itlalaguna.edu.mx>
2026-09-07 15:37:57 -0600

    Demo de temporizador

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,68 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tmrReloj = new System.Windows.Forms.Timer(this.components);
+            this.lblReloj = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblEjecucion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrReloj
+            // 
+            this.tmrReloj.Enabled = true;
+            this.tmrReloj.Interval = 1000;
+            this.tmrReloj.Tick += new System.EventHandler(this.tmrReloj_Tick);
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.AutoSize = true;
+            this.lblReloj.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.lblReloj.Location = new System.Drawing.Point(12, 9);
+            this.lblReloj.Name = "lblReloj";
+            this.lblReloj.Size = new System.Drawing.Size(63, 69);
+            this.lblReloj.TabIndex = 0;
+            this.lblReloj.Text = "0";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.lblFecha.Location = new System.Drawing.Point(12, 78);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(63, 69);
+            this.lblFecha.TabIndex = 1;
+            this.lblFecha.Text = "0";
+            // 
+            // lblEjecucion
+            // 
+            this.lblEjecucion.AutoSize = true;
+            this.lblEjecucion.Location = new System.Drawing.Point(13, 151);
+            this.lblEjecucion.Name = "lblEjecucion";
+            this.lblEjecucion.Size = new System.Drawing.Size(133, 16);
+            this.lblEjecucion.TabIndex = 2;
+            this.lblEjecucion.Text = "Tiempo en ejecución";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(443, 207);
+            this.Controls.Add(this.lblEjecucion);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblReloj);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmrReloj;
+        private System.Windows.Forms.Label lblReloj;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblEjecucion;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace myApp01
 {
     public partial class Form1 : Form
     {
+        int contador = 0, minutos = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tmrReloj_Tick(object sender, EventArgs e)
+        {
+            contador++;
+            DateTime tiempo = DateTime.Now;
+            lblReloj.Text = tiempo.ToString("HH:mm:ss");
+            lblFecha.Text = tiempo.ToString("MM-dd-yyyy");
+
+            if (contador == 60)
+            {
+                minutos++;
+                contador = 0;
+            }
+
+            lblEjecucion.Text = "Tiempo en ejecución: " + minutos.ToString();
         }
     }
 }
