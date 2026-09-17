4cb17791751970a2d1a8792f76a165499da58fda
Paco9CG <alu.25130444@correo.itlalaguna.edu.mx>
2026-09-17 15:35:30 -0600

    Datos con clase

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvInformacion = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mtbTelefono = new System.Windows.Forms.MaskedTextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btnAgregar);
+            this.splitContainer1.Panel1.Controls.Add(this.mtbTelefono);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.txtNombre);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvInformacion);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.splitContainer1.Size = new System.Drawing.Size(912, 450);
+            this.splitContainer1.SplitterDistance = 97;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgvInformacion
+            // 
+            this.dgvInformacion.AllowUserToAddRows = false;
+            this.dgvInformacion.AllowUserToDeleteRows = false;
+            this.dgvInformacion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInformacion.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvInformacion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInformacion.Location = new System.Drawing.Point(0, 0);
+            this.dgvInformacion.Name = "dgvInformacion";
+            this.dgvInformacion.ReadOnly = true;
+            this.dgvInformacion.RowHeadersWidth = 51;
+            this.dgvInformacion.RowTemplate.Height = 24;
+            this.dgvInformacion.Size = new System.Drawing.Size(912, 349);
+            this.dgvInformacion.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtNombre.Location = new System.Drawing.Point(12, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(371, 34);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No.";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 250;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Télefono";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 150;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(417, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Télefono";
+            // 
+            // mtbTelefono
+            // 
+            this.mtbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.mtbTelefono.Location = new System.Drawing.Point(422, 41);
+            this.mtbTelefono.Mask = "(999)000-0000";
+            this.mtbTelefono.Name = "mtbTelefono";
+            this.mtbTelefono.Size = new System.Drawing.Size(290, 34);
+            this.mtbTelefono.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnAgregar.Location = new System.Drawing.Point(718, 22);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(145, 53);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(912, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInformacion)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvInformacion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.MaskedTextBox mtbTelefono;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,31 @@ namespace myApp01
 {
     public partial class Form1 : Form
     {
+        List<Persona> personas = new List<Persona>();
+        
         public Form1()
         {
             InitializeComponent();
+            personas.Add(new Persona(1, "Francisco Hurtado", "8711668582"));
+            personas.Add(new Persona(2, "Yael Álvarez", "8711121314"));
+            personas.Add(new Persona(3, "Angel Betancourt", "8712121413"));
+            personas.Add(new Persona(4, "Jesús Valenzuela", "8712321452"));
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            dgvInformacion.Rows.Add();
+            dgvInformacion[0, dgvInformacion.Rows.Count - 1].Value = dgvInformacion.Rows.Count;
+            dgvInformacion[1, dgvInformacion.Rows.Count - 1].Value = txtNombre.Text;
+            dgvInformacion[2, dgvInformacion.Rows.Count - 1].Value = mtbTelefono.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(var Persona in personas)
+            {
+                dgvInformacion.Rows.Add(Persona.Id, Persona.nombre, Persona.telefono);
+            }
         }
     }
 }
